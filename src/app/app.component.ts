7ed5e8764aafaf3d80b5e26c6ec7cbded8826113
Moksh Jain <mokshjn00@gmail.com>
2017-05-24 14:17:30 +0530

    added toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MdDialog} from '@angular/material';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  loggedIn = false;
+
+  constructor(public dialog: MdDialog){}
+
+  viewAboutDialog() {
+    this.dialog.open(AboutComponent);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MdToolbarModule, MdIconModule, MdMenuModule, MdButtonModule, MdDialogModule } from '@angular/material';
+import { AboutComponent } from './components/about/about.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdMenuModule,
+    MdButtonModule,
+    MdDialogModule,
     AppRoutingModule
   ],
+  entryComponents: [AboutComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
